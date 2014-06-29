6e786bf93b1b8ba50c65844c74039032b80486d3
Dobiasd <harry@daiw.de>
2014-06-29 20:37:22 +0200

    remove whitespace from html

--- compile.sh
+++ compile.sh
@@ -4,7 +4,9 @@ elm -m --src-dir=./src --set-runtime=elm-runtime.js src/RedditTimeMachine.elm
 
 cp $HOME/.cabal/share/Elm-0.12.3/elm-runtime.js ./build
 
-mv ./build/src/RedditTimeMachine.html ./build/index.html
+#mv ./build/src/RedditTimeMachine.html ./build/index.html
+# remove whitespaces to shrink file size
+cat ./build/src/RedditTimeMachine.html | perl -pe "s/ +/ /g" > ./build/index.html
 
 cp -r ./src/icons ./build
 cp -r ./src/imgs ./build
