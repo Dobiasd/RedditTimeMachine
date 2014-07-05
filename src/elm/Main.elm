f719d1d2cd636f6132c8e0d6538089fb7816759f
Dobiasd <harry@daiw.de>
2014-07-05 13:48:15 +0200

    fix: only interval end needs to be a valid time in reddit history

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -178,7 +178,7 @@ showResult w rawName sfwOn nsfwOn criterion interval amount now
       Months -> (lastNMonthsSpans, String.dropRight 3)
       Years -> (lastNYearsSpans, String.dropRight 6)
     validTime x = x > 1114905600*second - 12*60*60*second -- 2005-05-01 minus 12 hours
-    spans = lastNFunc amount now |> filter (validTime . fst)
+    spans = lastNFunc amount now |> filter (validTime . snd)
     urls = map (genLink name criterion) spans
     texts = map (showTimeSpan transF timezoneOffset) spans
     spanCnt = length spans
