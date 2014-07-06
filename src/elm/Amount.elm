711a99a56c31d2b846727ba8a8eca9bdf5eadc81
Dobiasd <harry@daiw.de>
2014-07-06 15:13:59 +0200

    clamp input

--- src/elm/Amount.elm
+++ src/elm/Amount.elm
@@ -6,13 +6,13 @@ import SfwSwitches (toIntDef)
 type Amount = Int
 
 defaultAmount : Int
-defaultAmount = 10
+defaultAmount = 20
 
 amountInput : Input Amount
 amountInput = input defaultAmount
 
 readAmount : String -> Amount
-readAmount = toIntDef defaultAmount
+readAmount = max 10 . min 500 . toIntDef defaultAmount
 
 showAmount : Amount -> String
 showAmount = show
@@ -23,7 +23,7 @@ amountDropDown : Amount -> Element
 amountDropDown current =
   let
     f c = (showAmount c, c)
-    -- more than 1000 results in "too much recursion" for elm
-    all = [10, 20, 50, 100, 200, 500, 1000] |> filter (\x -> x /= current)
+    -- Too much results in "too much recursion" in firefox.
+    all = [10, 20, 50, 100, 200, 500] |> filter (\x -> x /= current)
   in
     dropDown amountInput.handle <| map f (current :: all)
