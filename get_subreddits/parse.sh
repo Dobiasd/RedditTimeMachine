d96190f4d85de6bbf1d104423307ee6faa67261f
Dobiasd <harry@daiw.de>
2014-09-29 07:21:06 +0200

    clean up

--- get_subreddits/parse.sh
+++ get_subreddits/parse.sh
@@ -1,6 +1,5 @@
 function parse {
-    rm temp_list.txt
-    grep -r $1 -e '.*' | grep -e "reddit.com\/r\/" | grep -v "%" | perl -pe 's/.*reddit.com.r.[^\/]*\/"\>([^\<]*).............([0-9]*).*/ , "\1,\2"/g' >> temp_list.txt
+    grep -r $1 -e '.*' | grep -e "reddit.com\/r\/" | grep -v "%" | perl -pe 's/.*reddit.com.r.[^\/]*\/"\>([^\<]*).............([0-9]*).*/ , "\1,\2"/g' > temp_list.txt
 
     echo module $2 where > $2.elm.temp
     echo "$1Raw : [String]" >> $2.elm.temp
