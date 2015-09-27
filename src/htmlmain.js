d016e881b727337dbd460fb5a74c6beb65a9c0b5
Dobiasd <harry@daiw.de>
2015-09-27 10:46:18 +0200

    cookie remark accoding to new EU law

--- src/htmlmain.js
+++ src/htmlmain.js
@@ -12,7 +12,64 @@ function GetTimezoneOffsetInMinutes() {
   var x = new Date();
   return x.getTimezoneOffset();
 }
+
+function AcceptCookies() {
+  if (lsTest() === true) {
+    try {
+      localStorage.setItem("cookieConsent", "true");
+    } catch(e) {
+    }
+  }
+  CheckCookieConsent();
+}
+
+function HideCookieRemark()
+{
+  document.getElementById('cookieRemark').style.height ='0px';
+  document.getElementById('cookieRemark').style.padding ='0px';
+  document.getElementById('cookieRemark').style.visibility = 'hidden';
+  document.getElementById('ads').style.top = '0px';
+}
+
+function ShowCookieRemark()
+{
+  document.getElementById('cookieRemark').style.height ='18px';
+  document.getElementById('cookieRemark').style.padding ='10px';
+  document.getElementById('cookieRemark').style.visibility = 'visible';
+  document.getElementById('ads').style.top = '38px';
+}
+
+// http://stackoverflow.com/questions/16427636/check-if-localstorage-is-available
+function lsTest(){
+  var test = 'test';
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch(e) {
+    return false;
+  }
+}
+
+function CheckCookieConsent() {
+  if (lsTest() === true) {
+    try {
+      cookieConsent = localStorage.getItem("cookieConsent");
+      if (cookieConsent == "true"){
+        HideCookieRemark();
+      }
+      else
+      {
+        ShowCookieRemark();
+      }
+    } catch(e) {
+      return "";
+    }
+  }
+}
+
 function Init() {
+  CheckCookieConsent();
   lastQuery = ""
   lastSearch = ""
   var query = getURLParameterDef("query", "");
