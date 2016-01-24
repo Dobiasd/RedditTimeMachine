c98385a4aa95d166ea1c7996f689671ec43aa8ac
Dobiasd <harry@daiw.de>
2016-01-24 21:18:17 +0100

    add favicon

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ then
   mkdir -p ./build/js
 
   cp ./src/index.html ./build/index.html
+  cp ./src/favicon.png ./build/favicon.png
   uglifyjs ./build/js/rtm_uncompressed.js > ./build/js/rtm.js
   rm ./build/js/rtm_uncompressed.js
   uglifyjs ./src/htmlmain.js > ./build/js/htmlmain.js
